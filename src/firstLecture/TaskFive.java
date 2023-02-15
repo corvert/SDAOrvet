a9fad6bfa531a984e588e82a3ae98d0707d1f662
Orvet Priimägi <orvet.priimagi@gmail.com>
2023-02-15 08:08:00 +0200

    First Lecture final

--- src/firstLecture/TaskFive.java
+++ src/firstLecture/TaskFive.java
@@ -7,22 +7,20 @@ public class TaskFive {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter number");
         int n = sc.nextInt();
-        //int m = n / 2;
-        int isPrime;
+        int isPrime = 0;
         for (int i = 0; i <= n; i++) {
             if (i == 0 || i == 1)
                 continue;
-            isPrime = 1;
-            for (int j = 2; j < i/2; ++j){
-                if (i % j == 0){
+                isPrime = 1;
+            for (int j = 2; j < i; j++) {
+                if (i % j == 0) {
                     isPrime = 0;
                     break;
                 }
-
             }
-            if (isPrime==1)
-            System.out.println(i);
+            if (isPrime == 1) {
+                System.out.println(i);
+            }
         }
-
     }
 }
